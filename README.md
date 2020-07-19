3f4b187ee3fc6a29470eed8def949956822df9d8
ezkemboi <ezrqnkemboi@gmail.com>
2020-07-19 23:09:04 +0300

    add single post login

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # kemboijstest
 This is test application to test kemboijs(Nodejs API-based framework)
+
+# Endpoints 
+
+- `/login`
--- index.js
+++ index.js
@@ -1,7 +1,50 @@
-const kemboijs = require('kemboijs');
+const Server = require('kemboijs')
 
-function start() {
-    return kemboijs
-}
+const app = new Server()
 
-start()
+const users = [
+    {
+        name: "kipmurkor",
+        password: "password"
+    },
+    {
+        name: "kemboi",
+        password: "password"
+    },
+    {
+        name: "evans",
+        password: "password"
+    }
+]
+
+app.post('/login', async (req, res) => {
+    const { username, password } = req.body;
+
+    if(!username || !password) {
+        return res.send({
+            message: 'Provide username and password'
+        }, 400)
+    }
+
+    const user = users.find(user => user.name === username);
+
+    if(!user) {
+        return res.send({
+            message: "User does not exist"
+        }, 400)
+    }
+
+    if(user.password !== password) {
+        return res.send({
+            message: "Password does not match"
+        }, 400)
+    }
+    
+    return res.send({
+        message: "logged in successfully"
+    }, 200)
+})
+
+app.listen(8000, () => {
+    console.log(`I am listening to http://localhost:8000`)
+})
